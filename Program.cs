1ca0bae98d7662bb741b16a0bbfb9914ece522b9
hro1025 <hro102025@gmail.com>
2025-11-11 12:40:02 +0100

    new

--- Program.cs
+++ Program.cs
@@ -31,16 +31,54 @@ class Program
             else if (input == "ja" || input == "Ja")
             {
                 AnsiConsole.MarkupLine("[bold]Great[/], lets go :)\nHva er navnet ditt?");
-                string? input2 = Console.ReadLine();
-
-                AnsiConsole.MarkupLine("[bold]Super[/] hva med din alder?");
-
-                string? input3 = Console.ReadLine();
-
-                if (int.TryParse(input3, out int alder)) { }
-                else
+                string? name = Console.ReadLine();
+                int dinAlder;
+                while (true)
                 {
-                    AnsiConsole.MarkupLine("[bold red]Det er kun tall[/]");
+                    AnsiConsole.MarkupLine("Hva med din alder?");
+                    string? alder = Console.ReadLine();
+
+                    if (int.TryParse(alder, out dinAlder))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        AnsiConsole.MarkupLine("[bold red]Det er kun tall!!![/]");
+                    }
+                }
+
+                AnsiConsole.MarkupLine("Hvor er det du bor?");
+                string? bor = Console.ReadLine();
+
+                AnsiConsole.MarkupLine(
+                    "[bold]Takk for informasjonen[/]\nVent litt mens jeg kalkulere din input"
+                );
+                AnsiConsole.Markup("[bold]Loading [/]");
+                Thread.Sleep(500);
+                AnsiConsole.Markup("-");
+                Thread.Sleep(200);
+                AnsiConsole.Markup("-");
+                Thread.Sleep(200);
+                AnsiConsole.Markup("-");
+                Thread.Sleep(200);
+                AnsiConsole.Markup("-");
+                Thread.Sleep(200);
+                AnsiConsole.Markup("-");
+                Thread.Sleep(200);
+                AnsiConsole.Markup("-");
+                Thread.Sleep(200);
+
+                AnsiConsole.MarkupLine(" [green] Finished[/]");
+                Thread.Sleep(500);
+
+                switch (dinAlder)
+                {
+                    case < 20:
+                    {
+                        Console.WriteLine("good");
+                        break;
+                    }
                 }
 
                 repeatProgram = false;
